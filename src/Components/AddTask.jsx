b835174a67c25bda6ec96ca9f5f7b902f6867a62
sri-ram-ravi <sriramravigit@gmail.com>
2024-01-30 12:04:44 +0530

    Added bootstrap css for the Add Task component

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <>
+    <div class="row mt-3">
+        <div class="col">
+          <input type="text" class="form-control p-2" placeholder="Add Todo" aria-label="First name"/>
+        </div>
+        <div class="col">
+        <button type="submit" class="btn btn-primary p-2">Submit</button>
+        </div>
+    </div>
+    </>
   )
 }
 
