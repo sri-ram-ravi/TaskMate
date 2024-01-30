ad93cc72c69b4dccedce6c85cafe96e4ba9356d6
sri-ram-ravi <sriramravigit@gmail.com>
2024-01-30 11:29:39 +0530

    Created Component and used design on header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <nav class="navbar bg-success p-2 text-white">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <h1 class="text-white">Task Mate</h1>
+          </a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
