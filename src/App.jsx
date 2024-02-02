dfee2024aa72ca32a9a23ddd2901949ae8512643
sri-ram-ravi <sriramravigit@gmail.com>
2024-02-02 13:54:44 +0530

    created a local storage for the task list and stored it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Components/Header'
 import AddTask from './Components/AddTask'
 import ShowTask from './Components/ShowTask'
 
 const App = () => {
-  const [taskList,setTaskList]=useState([]);
+  const [taskList,setTaskList]=useState(JSON.parse(localStorage.getItem("task-item")) || []);
   const [updateTask,setUpdateTask]=useState({});
+
+  useEffect(()=>{
+    localStorage.setItem("task-item",JSON.stringify(taskList))
+  },[taskList])
+
   return (
     <div>
         <Header/>
